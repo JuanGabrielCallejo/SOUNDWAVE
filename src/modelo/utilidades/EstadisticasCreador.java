713e26ba5e282355eedf17d5b17a942b5dc4b6d2
Juan Gabriel Callejo Vidal <a25juangcv@teis.intranet>
2026-02-12 18:14:55 +0100

    fin del esqueleto

---
+++ src/modelo/utilidades/EstadisticasCreador.java
@@ -0,0 +1,118 @@
+package modelo.utilidades;
+
+import modelo.artistas.Creador;
+import modelo.contenido.Podcast;
+
+import java.util.HashMap;
+
+public class EstadisticasCreador {
+
+    private Creador creador;
+    private int totalEpisodios;
+    private int totalReproducciones;
+    private double promedioReproducciones;
+    private int totalSuscriptores;
+    private int totalLikes;
+    private int duracionTotalSegundos;
+    private Podcast episodioMasPopular;
+    private HashMap<Integer, Integer> episodiosPorTemporada;
+
+
+    public Creador getCreador() {
+        return creador;
+    }
+
+    public void setCreador(Creador creador) {
+        this.creador = creador;
+    }
+
+    public int getTotalEpisodios() {
+        return totalEpisodios;
+    }
+
+    public void setTotalEpisodios(int totalEpisodios) {
+        this.totalEpisodios = totalEpisodios;
+    }
+
+    public int getTotalReproducciones() {
+        return totalReproducciones;
+    }
+
+    public void setTotalReproducciones(int totalReproducciones) {
+        this.totalReproducciones = totalReproducciones;
+    }
+
+    public double getPromedioReproducciones() {
+        return promedioReproducciones;
+    }
+
+    public void setPromedioReproducciones(double promedioReproducciones) {
+        this.promedioReproducciones = promedioReproducciones;
+    }
+
+    public int getTotalSuscriptores() {
+        return totalSuscriptores;
+    }
+
+    public void setTotalSuscriptores(int totalSuscriptores) {
+        this.totalSuscriptores = totalSuscriptores;
+    }
+
+    public int getTotalLikes() {
+        return totalLikes;
+    }
+
+    public void setTotalLikes(int totalLikes) {
+        this.totalLikes = totalLikes;
+    }
+
+    public int getDuracionTotalSegundos() {
+        return duracionTotalSegundos;
+    }
+
+    public void setDuracionTotalSegundos(int duracionTotalSegundos) {
+        this.duracionTotalSegundos = duracionTotalSegundos;
+    }
+
+    public Podcast getEpisodioMasPopular() {
+        return episodioMasPopular;
+    }
+
+    public void setEpisodioMasPopular(Podcast episodioMasPopular) {
+        this.episodioMasPopular = episodioMasPopular;
+    }
+
+    public HashMap<Integer, Integer> getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(HashMap<Integer, Integer> episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public EstadisticasCreador(Creador creador) {
+        this.creador = creador;
+    }
+
+    private void calcularEstadisticas(){}
+
+    private String formatearDuracion(int segundos){
+        return null;
+    }
+
+    public String generarReporte(){
+        return null;
+    }
+
+    public double calcularEngagement(){
+        return 0;
+    }
+
+    public int estimarCrecimientoMensual(){ return 0; };
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
--- src/modelo/utilidades/RecomendadorIA.java
+++ src/modelo/utilidades/RecomendadorIA.java
@@ -1,8 +1,10 @@
 package modelo.utilidades;
 
 import enums.AlgoritmoRecomendacion;
+import excepciones.recomendacion.RecomendacionException;
 import interfaces.Recomendador;
 import modelo.contenido.Contenido;
+import modelo.usuarios.Usuario;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,10 +21,92 @@ public class RecomendadorIA implements Recomendador {
     private static final double UMBRAL_DEFAULT = 0.6;
 
 
+    public HashMap<String, ArrayList<String>> getMatrizPreferencias() {
+        return matrizPreferencias;
+    }
+
+    public void setMatrizPreferencias(HashMap<String, ArrayList<String>> matrizPreferencias) {
+        this.matrizPreferencias = matrizPreferencias;
+    }
+
+    public HashMap<String, ArrayList<Contenido>> getHistorialalCompleto() {
+        return historialalCompleto;
+    }
+
+    public void setHistorialalCompleto(HashMap<String, ArrayList<Contenido>> historialalCompleto) {
+        this.historialalCompleto = historialalCompleto;
+    }
+
+    public AlgoritmoRecomendacion getAlgoritmo() {
+        return algoritmo;
+    }
+
+    public void setAlgoritmo(AlgoritmoRecomendacion algoritmo) {
+        this.algoritmo = algoritmo;
+    }
+
+    public double getUmbralSimilitud() {
+        return umbralSimilitud;
+    }
+
+    public void setUmbralSimilitud(double umbralSimilitud) {
+        this.umbralSimilitud = umbralSimilitud;
+    }
+
+    public boolean isModeloEntrenado() {
+        return modeloEntrenado;
+    }
+
+    public void setModeloEntrenado(boolean modeloEntrenado) {
+        this.modeloEntrenado = modeloEntrenado;
+    }
+
+    public ArrayList<Contenido> getCatalogoReferencia() {
+        return catalogoReferencia;
+    }
+
+    public void setCatalogoReferencia(ArrayList<Contenido> catalogoReferencia) {
+        this.catalogoReferencia = catalogoReferencia;
+    }
+
     public RecomendadorIA() {
     }
 
     public RecomendadorIA(AlgoritmoRecomendacion algoritmo) {
         this.algoritmo = algoritmo;
     }
+
+
+    @Override
+    public ArrayList<Contenido> recomendar(Usuario usuario) throws RecomendacionException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Contenido> obetenerSimilares(Contenido contenido) throws RecomendacionException {
+        return null;
+    }
+
+    public void entrenarModelo(ArrayList<Usuario>usuarios){
+
+    }
+
+    public void entrenarModelo(ArrayList<Usuario> usuarios, ArrayList<Contenido> catalogo){}
+
+    private double calcularSimilitud(Usuario u1, Usuario u2){
+        return 0;
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+
+
+
+
+
+
 }
